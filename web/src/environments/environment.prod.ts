b800bf99aff3dca992cef28823a94c8ef26e437c
Mike Nefedjev <nmwind>
2019-12-06 15:28:07 +0300

    add firebase

--- web/src/environments/environment.prod.ts
+++ web/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCoD4hTrcTJjG-mnF1Z-qk8lZiHL47ljrc",
+    authDomain: "jtrade.firebaseapp.com",
+    databaseURL: "https://jtrade.firebaseio.com",
+    projectId: "jtrade",
+    storageBucket: "jtrade.appspot.com",
+    messagingSenderId: "294993511528",
+    appId: "1:294993511528:web:0af9512e4e95dbd130e7dd",
+    measurementId: "G-KE0LETSZG4"
+  }
 };
--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyCoD4hTrcTJjG-mnF1Z-qk8lZiHL47ljrc",
+    authDomain: "jtrade.firebaseapp.com",
+    databaseURL: "https://jtrade.firebaseio.com",
+    projectId: "jtrade",
+    storageBucket: "jtrade.appspot.com",
+    messagingSenderId: "294993511528",
+    appId: "1:294993511528:web:0af9512e4e95dbd130e7dd",
+    measurementId: "G-KE0LETSZG4"
+  }
 };
 
 /*
